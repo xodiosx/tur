9d0c9fc27001a16bee8d8be824c23815d67047ff
Mody <mohammedaleiwi@gmail.com>
2026-08-19 21:29:28 +0300

    Update build.sh

--- tur-on-device/ppsspp/build.sh
+++ tur-on-device/ppsspp/build.sh
@@ -1,14 +1,13 @@
 TERMUX_PKG_HOMEPAGE=https://www.ppsspp.org/
 TERMUX_PKG_DESCRIPTION="PlayStation Portable emulator"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_EXCLUDED_ARCHES="arm, i686"
 TERMUX_PKG_VERSION="1.20.4"
 TERMUX_PKG_GIT_BRANCH="v${TERMUX_PKG_VERSION}"
-TERMUX_PKG_EXCLUDED_ARCHES="arm, i686, x86_64"
+TERMUX_PKG_EXCLUDED_ARCHES="arm i686 x86_64"
 TERMUX_PKG_SRCURL="git+https://github.com/hrydgard/ppsspp"
 TERMUX_PKG_DEPENDS="libcurl, libpng, miniupnpc, zlib, libzip, libsnappy, ffmpeg, sdl2, sdl2-ttf"
 TERMUX_PKG_BUILD_DEPENDS="mesa-dev, libglvnd-dev, vulkan-headers, rapidjson, spirv-headers, spirv-tools"
-TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_AUTO_UPDATE=false
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DCMAKE_SYSTEM_NAME=Linux
 -DCMAKE_SYSTEM_PROCESSOR=aarch64
@@ -16,6 +15,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DUSING_GLES2=OFF
 -DBUILD_TESTING=OFF
 "
+
 termux_step_pre_configure() {
 	cd "$TERMUX_PKG_SRCDIR"
 	sed -i '120s/.*/#elif 0/' ppsspp_config.h
