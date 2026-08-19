9ae83903b2903641d5b3df5f81838bc6c0fa99d7
Mody <mohammedaleiwi@gmail.com>
2026-08-19 23:13:21 +0300

    Update build.sh

--- tur-on-device/retroarch/build.sh
+++ tur-on-device/retroarch/build.sh
@@ -4,7 +4,7 @@ TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
 TERMUX_PKG_VERSION="1.19.1"
 TERMUX_PKG_REVISION=1
-TERMUX_PKG_EXCLUDED_ARCHES="arm, i686"
+TERMUX_PKG_EXCLUDED_ARCHES="arm, i686, x86_64"
 TERMUX_PKG_SRCURL="https://github.com/libretro/RetroArch/archive/v${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256="504a3a8a6e5861eb43a61be8339f61183e7ea940c1ff68ac2a2f57d35c67f8ff"
 TERMUX_PKG_DEPENDS="libandroid-shmem, fontconfig, libzip, libx11, libxrandr, libxext, freetype, pulseaudio, sdl2, sdl2-ttf, libglvnd, ffmpeg, libsixel, zlib, libxcb, vulkan-loader"
@@ -30,4 +30,8 @@ termux_step_pre_configure() {
 	export CFLAGS="-I$TERMUX_PREFIX/include"
 	export CPPFLAGS="-I$TERMUX_PREFIX/include"
 	export LDFLAGS+=" -L$TERMUX_PREFIX/lib -landroid-shmem"
+}
+
+termux_step_configure() {
+	./configure --prefix="$TERMUX_PREFIX" $TERMUX_PKG_EXTRA_CONFIGURE_ARGS
 }
