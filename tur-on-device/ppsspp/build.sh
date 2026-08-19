72970bdc819fa1a5d84bbfbcf1a1e7c2a6b096bf
Mody <mohammedaleiwi@gmail.com>
2026-08-20 02:37:26 +0300

    Update build script for PPSSPP with new configurations

--- tur-on-device/ppsspp/build.sh
+++ tur-on-device/ppsspp/build.sh
@@ -11,9 +11,12 @@ TERMUX_PKG_BUILD_DEPENDS="mesa-dev, libglvnd-dev, vulkan-headers, rapidjson, spi
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DCMAKE_SYSTEM_NAME=Linux
--DCMAKE_SYSTEM_PROCESSOR=aarch64
 -DUSE_SYSTEM_FFMPEG=ON
+-DUSE_SYSTEM_LIBZIP=ON
+-DUSE_SYSTEM_SNAPPY=ON
 -DUSING_GLES2=OFF
+-DUSING_QT_UI=OFF
+-DHEADLESS=OFF
 -DBUILD_TESTING=OFF
 -DCMAKE_PREFIX_PATH=${TERMUX_PREFIX}
 -DSDL2_INCLUDE_DIR=${TERMUX_PREFIX}/include/SDL2
@@ -28,16 +31,25 @@ termux_step_pre_configure() {
 	#CXXFLAGS+=" -DGLEW_NO_GLU"
 
 	cd "$TERMUX_PKG_SRCDIR"
-	sed -i '120s/.*/#elif 0/' ppsspp_config.h
-	sed -i '403s/.*/#if 0/' Common/GPU/OpenGL/GLFeatures.cpp
-	sed -i '456s/.*/#if 0/' Common/GPU/OpenGL/GLFeatures.cpp
-	sed -i '194s/.*/#if 0/' Common/GPU/Vulkan/VulkanLoader.h
-	sed -i '192s/.*/#if 0/' Common/GPU/Vulkan/VulkanLoader.cpp
-	sed -i '350s/.*/#if 1/' Common/GPU/Vulkan/VulkanLoader.cpp
-	sed -i '503s/.*/#elif 0/' Common/GPU/Vulkan/VulkanLoader.cpp
-	sed -i '752s/.*/#elif 0/' Common/GPU/Vulkan/VulkanLoader.cpp
-	sed -i '155s/.*/#elif 0/' Common/GPU/Vulkan/VulkanContext.cpp
-	sed -i '1063s/.*/#if 0/' Common/GPU/Vulkan/VulkanContext.cpp
-	sed -i '145s/.*/#elif 1/' Core/Instance.cpp
-	sed -i '182s/.*/#elif 1/' Core/Instance.cpp
+	#sed -i '120s/.*/#elif 0/' ppsspp_config.h
+	#sed -i '403s/.*/#if 0/' Common/GPU/OpenGL/GLFeatures.cpp
+	#sed -i '456s/.*/#if 0/' Common/GPU/OpenGL/GLFeatures.cpp
+	#sed -i '194s/.*/#if 0/' Common/GPU/Vulkan/VulkanLoader.h
+	#sed -i '192s/.*/#if 0/' Common/GPU/Vulkan/VulkanLoader.cpp
+	#sed -i '350s/.*/#if 1/' Common/GPU/Vulkan/VulkanLoader.cpp
+	#sed -i '503s/.*/#elif 0/' Common/GPU/Vulkan/VulkanLoader.cpp
+	#sed -i '752s/.*/#elif 0/' Common/GPU/Vulkan/VulkanLoader.cpp
+	#sed -i '155s/.*/#elif 0/' Common/GPU/Vulkan/VulkanContext.cpp
+	#sed -i '1063s/.*/#if 0/' Common/GPU/Vulkan/VulkanContext.cpp
+	#sed -i '145s/.*/#elif 1/' Core/Instance.cpp
+	#sed -i '182s/.*/#elif 1/' Core/Instance.cpp
+}
+
+termux_step_post_make_install() {
+	install -Dm700 "$TERMUX_PKG_BUILDDIR/PPSSPPSDL" \
+		"$TERMUX_PREFIX/bin/PPSSPPSDL"
+
+	# PPSSPP looks for assets/ relative to its own binary.
+	mkdir -p "$TERMUX_PREFIX/share/ppsspp"
+	cp -r "$TERMUX_PKG_BUILDDIR/assets" "$TERMUX_PREFIX/share/ppsspp/"
 }
