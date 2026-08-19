1e3c527b6a58078e296beb132d0e47f843e418f2
Mody <mohammedaleiwi@gmail.com>
2026-08-19 21:46:41 +0300

    Update build script for PPSSPP emulator

--- tur-on-device/ppsspp/build.sh
+++ tur-on-device/ppsspp/build.sh
@@ -1,13 +1,14 @@
 TERMUX_PKG_HOMEPAGE=https://www.ppsspp.org/
 TERMUX_PKG_DESCRIPTION="PlayStation Portable emulator"
 TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_MAINTAINER="@termux-user-repository"
 TERMUX_PKG_VERSION="1.20.4"
 TERMUX_PKG_GIT_BRANCH="v${TERMUX_PKG_VERSION}"
-TERMUX_PKG_EXCLUDED_ARCHES="arm i686 x86_64"
 TERMUX_PKG_SRCURL="git+https://github.com/hrydgard/ppsspp"
+TERMUX_PKG_EXCLUDED_ARCHES="arm, i686, x86_64"
 TERMUX_PKG_DEPENDS="libcurl, libpng, miniupnpc, zlib, libzip, libsnappy, ffmpeg, sdl2, sdl2-ttf"
 TERMUX_PKG_BUILD_DEPENDS="mesa-dev, libglvnd-dev, vulkan-headers, rapidjson, spirv-headers, spirv-tools"
-TERMUX_PKG_AUTO_UPDATE=false
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DCMAKE_SYSTEM_NAME=Linux
 -DCMAKE_SYSTEM_PROCESSOR=aarch64
