0564182b3109c605924f3fb3d719da027dbbd3ba
Mody <mohammedaleiwi@gmail.com>
2026-08-21 20:54:30 +0300

    Update build.sh

--- tur-on-device/ppsspp/build.sh
+++ tur-on-device/ppsspp/build.sh
@@ -56,7 +56,6 @@ termux_step_pre_configure() {
 	cd "$TERMUX_PKG_SRCDIR"
 	sed -i 's/Arm64EmitterTest();/\/\/ Arm64EmitterTest();/' UI/NativeApp.cpp
 	sed -i 's/ArmEmitterTest();/\/\/ ArmEmitterTest();/' UI/NativeApp.cpp
-	sed -i 's/install(PROGRAMS external-ip.sh/install(CODE "MESSAGE(\\"Skipping external-ip.sh\\")" # PROGRAMS external-ip.sh/' file
 	# Patch cpu_features so empty CMAKE_SYSTEM_PROCESSOR is inferred from compiler
 	sed -i '/set(PROCESSOR_IS_LOONGARCH FALSE)/a\
 if(CMAKE_SYSTEM_PROCESSOR STREQUAL "")\
