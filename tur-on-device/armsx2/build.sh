123318b6e7c3a1d234b80040f9d4eef998d1f389
Mody <mohammedaleiwi@gmail.com>
2026-09-15 13:56:13 +0300

    Update build dependencies and Wayland configuration

--- tur-on-device/armsx2/build.sh
+++ tur-on-device/armsx2/build.sh
@@ -6,13 +6,13 @@ TERMUX_PKG_VERSION="2.6.9"
 TERMUX_PKG_SRCURL="git+https://github.com/ARMSX2/ARMSX2.git"
 TERMUX_PKG_GIT_BRANCH="main"
 TERMUX_PKG_DEPENDS="libc++, sdl2, ffmpeg, zstd, libcurl, freetype, libpng, libjpeg-turbo, libwebp, liblzma, vulkan-loader, libandroid-shmem, wayland, libx11, qt6-qtbase, qt6-qtwayland, libaio, libsoundtouch, libzip, shaderc"
-TERMUX_PKG_BUILD_DEPENDS="cmake, ninja, pkg-config, vulkan-headers"
+TERMUX_PKG_BUILD_DEPENDS="mesa-dev, cmake, ninja, pkg-config, vulkan-headers"
 TERMUX_PKG_BUILD_IN_SRC=false
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DCMAKE_BUILD_TYPE=Release
 -DCMAKE_SYSTEM_NAME=Linux
--DUSE_WAYLAND=ON
+-DUSE_WAYLAND=OFF
 -DUSE_X11=ON
 -DUSE_VULKAN=ON
 -DUSE_QT=ON
