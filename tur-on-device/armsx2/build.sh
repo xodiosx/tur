779b98b4ad594b049fc207518f70542385f553bb
Mody <mohammedaleiwi@gmail.com>
2026-09-16 02:08:39 +0300

    Integrate plutosvg installation in build script
    Added steps to compile and install plutosvg during the build process.

--- tur-on-device/armsx2/build.sh
+++ tur-on-device/armsx2/build.sh
@@ -6,7 +6,7 @@ TERMUX_PKG_VERSION="2.6.9"
 TERMUX_PKG_SRCURL="git+https://github.com/ARMSX2/ARMSX2.git"
 TERMUX_PKG_GIT_BRANCH="master"
 TERMUX_PKG_EXCLUDED_ARCHES="arm i686 x86_64"
-TERMUX_PKG_DEPENDS="libc++, sdl3, ffmpeg, zstd, libcurl, freetype, libpng, libjpeg-turbo, libwebp, liblzma, vulkan-loader, libandroid-shmem, libx11, qt6-qtbase, libaio, libsoundtouch, libzip, shaderc, plutovg, plutosvg"
+TERMUX_PKG_DEPENDS="libc++, sdl3, ffmpeg, zstd, libcurl, freetype, libpng, libjpeg-turbo, libwebp, liblzma, vulkan-loader, libandroid-shmem, libx11, qt6-qtbase, libaio, libsoundtouch, libzip, shaderc, plutovg"
 TERMUX_PKG_BUILD_DEPENDS="cmake, ninja, pkg-config, vulkan-headers"
 TERMUX_PKG_BUILD_IN_SRC=false
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
@@ -23,6 +23,18 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 termux_step_pre_configure() {
 	sed -i '/function(detect_page_size)/a \ \ set(PAGE_SIZE 4096 PARENT_SCOPE)\n \ return()' "${TERMUX_PKG_SRCDIR}/cmake/Pcsx2Utils.cmake"
 	sed -i '/function(detect_cache_line_size)/a \ \ set(CACHE_LINE_SIZE 64 PARENT_SCOPE)\n \ return()' "${TERMUX_PKG_SRCDIR}/cmake/Pcsx2Utils.cmake"
+
+	# Compile and install plutosvg into $TERMUX_PREFIX
+	local PLUTOSVG_SRC="${TERMUX_PKG_CACHEDIR}/plutosvg"
+	if [ ! -d "$PLUTOSVG_SRC" ]; then
+		git clone --recursive https://github.com/sammycage/plutosvg.git "$PLUTOSVG_SRC"
+	fi
+	cmake -S "$PLUTOSVG_SRC" -B "$PLUTOSVG_SRC/build" \
+		-DCMAKE_INSTALL_PREFIX="$TERMUX_PREFIX" \
+		-DCMAKE_BUILD_TYPE=Release \
+		-DPLUTOSVG_BUILD_EXAMPLES=OFF
+	cmake --build "$PLUTOSVG_SRC/build" --target install
+
 	LDFLAGS+=" -landroid-shmem"
 	CFLAGS+=" -fPIE"
 	CXXFLAGS+=" -fPIE"
